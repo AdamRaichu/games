9aed7a2144ebf14b87104892218e6952d909728a
AdamRaichu <91494019+AdamRaichu@users.noreply.github.com>
2022-01-11 18:58:20 -0500

    Update game.js

--- tycoon/game.js
+++ tycoon/game.js
@@ -1,5 +1,20 @@
 //  /tycoon/game.js
-var Game = function () {
+var GAME = function () {
   this.currency = 100
   this.playerName = prompt("Choose a username.")
 }
+
+GAME.prototype.update = function() {
+  counter.innerText = this.currency
+}
+
+GAME.prototype.earn = function (amount) {
+  this.currency += amount
+}
+
+var counter = document.getElementById("status_counter")
+var buttons = {
+  click: document.getElementById("buttons:click")
+}
+
+setInterval(Game.update(), 1000)
