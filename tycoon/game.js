b753725f1c05e8eab9206951f9e020bbfac884c4
AdamRaichu <91494019+AdamRaichu@users.noreply.github.com>
2022-01-13 13:22:53 -0500

    Update game.js

--- tycoon/game.js
+++ tycoon/game.js
@@ -5,12 +5,35 @@ var GAME = function () {
   } else {
     this.currency = 100
   }
+  
+  if (localStorage.owned) {
+    this.owned = localStorage.getVar("owned")
+  } else {
+    this.owned = []
+  }
+  for (checkList = 0; checkList < this.owned.length; checkList++) {
+    this.owned[checkList].dataset.owned = true
+  }
+  this.clickEvents = {
+    default: function(){Game.earn(1)},
+    buy1: function () {
+      if (buttons.buy1.dataset.owned) {
+        Game.earn(5)
+      } else {
+        if (Game.currency >= 100) {
+          Game.earn(-100)
+          buttons.buy1.dataset.owned = true
+        } else {
+          Game.lackOfFunds()
+        }
+      }
+    }
+  }
 }
 
 GAME.prototype.update = function() {
   document.getElementById("status_counter").innerHTML = this.currency
   localStorage.setVar("currency", this.currency)
-  buttons.click.blur()
 }
 
 GAME.prototype.earn = function (amount) {
@@ -18,7 +41,12 @@ GAME.prototype.earn = function (amount) {
 }
 
 var buttons = {
-  click: document.getElementById("buttons_click")
+  click: document.getElementById("buttons_click"),
+  buy1: document.getElementById("buttons_buy1")
+}
+
+Game.prototype.lackOfFunds = function () {
+  //
 }
 
 var Game = new GAME()
