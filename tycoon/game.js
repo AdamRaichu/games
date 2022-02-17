09a97537e3ffbafb0e44632e6922f57c5ace860b
AdamRaichu <91494019+AdamRaichu@users.noreply.github.com>
2022-02-17 15:38:14 -0500

    Update game.js

--- tycoon/game.js
+++ tycoon/game.js
@@ -1,60 +1,61 @@
-//  /tycoon/game.js
-var GAME = function () {
-  if (localStorage.currency) {
-    this.currency = localStorage.getVar("currency")
-  } else {
-    this.currency = 100
-  }
-  
-  if (localStorage.owned) {
-    this.owned = localStorage.getVar("owned")
-  } else {
-    this.owned = []
-  }
-  for (checkList = 0; checkList < this.owned.length; checkList++) {
-    this.owned[checkList].dataset.owned = true
-  }
-  this.clickEvents = {
-    default: function(){Game.earn(1)},
-    buy1: function () {
-      if (buttons.buy1.dataset.owned === 'true') {
-        Game.earn(5)
-      } else {
-        if (Game.currency >= 100) {
-          Game.earn(-100)
-          buttons.buy1.dataset.owned = true
+function begin() {//  /tycoon/game.js
+  var GAME = function () {
+    if (localStorage.currency) {
+      this.currency = localStorage.getVar("currency")
+    } else {
+      this.currency = 100
+    }
+    
+    if (localStorage.owned) {
+      this.owned = localStorage.getVar("owned")
+    } else {
+      this.owned = []
+    }
+    for (checkList = 0; checkList < this.owned.length; checkList++) {
+      this.owned[checkList].dataset.owned = true
+    }
+    this.clickEvents = {
+      default: function(){Game.earn(1)},
+      buy1: function () {
+        if (buttons.buy1.dataset.owned === 'true') {
+          Game.earn(5)
         } else {
-          Game.lackOfFunds()
+          if (Game.currency >= 100) {
+            Game.earn(-100)
+            buttons.buy1.dataset.owned = true
+          } else {
+            Game.lackOfFunds()
+          }
         }
       }
     }
+    buttons.buy1.dataset.owned = localStorage.buttons_buy1_owned
+    if (buttons.buy1.dataset.owned) {
+      buttons.buy1.innerText = "Click ($5)"
+    } else {
+      buttons.buy1.innerText = "Buy ($100); $5 per click"
   }
-  buttons.buy1.dataset.owned = localStorage.buttons_buy1_owned
-  if (buttons.buy1.dataset.owned) {
-    buttons.buy1.innerText = "Click ($5)"
-  } else {
-    buttons.buy1.innerText = "Buy ($100); $5 per click"
   }
+  
+  GAME.prototype.update = function() {
+    document.getElementById("status_counter").innerHTML = this.currency
+    localStorage.setVar("currency", this.currency)
+    localStorage.setItem("buttons_buy1_owned", buttons.buy1.dataset.owned)
+  }
+  
+  GAME.prototype.earn = function (amount) {
+    this.currency += amount
+  }
+  
+  var buttons = {
+    click: document.getElementById("buttons_click"),
+    buy1: document.getElementById("buttons_buy1")
+  }
+  
+  GAME.prototype.lackOfFunds = function () {
+    //
+  }
+  
+  var Game = new GAME()
+  Game.update()
 }
-
-GAME.prototype.update = function() {
-  document.getElementById("status_counter").innerHTML = this.currency
-  localStorage.setVar("currency", this.currency)
-  localStorage.setItem("buttons_buy1_owned", buttons.buy1.dataset.owned)
-}
-
-GAME.prototype.earn = function (amount) {
-  this.currency += amount
-}
-
-var buttons = {
-  click: document.getElementById("buttons_click"),
-  buy1: document.getElementById("buttons_buy1")
-}
-
-GAME.prototype.lackOfFunds = function () {
-  //
-}
-
-var Game = new GAME()
-Game.update()
