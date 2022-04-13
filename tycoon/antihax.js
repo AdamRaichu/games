9b363c1b6065205f303c2c9fb2148939297122f2
AdamRaichu <91494019+AdamRaichu@users.noreply.github.com>
2022-04-13 13:12:03 -0400

    Delete tycoon directory

--- tycoon/antihax.js
+++
@@ -1,4 +0,0 @@
-setInterval(function() {
-  buttons.click.setAttribute("onclick", "Game.clickEvents.default(); Game.update(); this.blur()")
-  buttons.buy1.setAttribute("onclick", "Game.clickEvents.buy1(); Game.update(); this.blur()")
-}, 100);
