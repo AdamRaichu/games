a45a88ccde95a99250864f1173544a1ebd9e15ee
AdamRaichu <91494019+AdamRaichu@users.noreply.github.com>
2022-04-13 15:14:20 -0400

    added the rest of the types

--- pokemon/functions.js
+++ pokemon/functions.js
@@ -1,8 +1,24 @@
 function typeToString (number) {
 	if (number === 0) {
 		return "grass"
+	} else if (number === 1) {
+		return "fire"
+	} else if (number === 2) {
+		return "water"
 	} else if (number === 3) {
 		return "electric"
+	} else if (number === 4) {
+		return "psychic"
+	} else if (number === 5) {
+		return "fighting"
+	} else if (number === 6) {
+		return "dark"
+	} else if (number === 7) {
+		return "steel"
+	} else if (number === 8) {
+		return "fairy"
+	} else if (number === 9) {
+		return "dragon"
 	} else if (number === 10) {
 		return "colorless"
 	}
