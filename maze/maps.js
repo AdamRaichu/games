d94df6944d28edc63c39a3bb78c7486c580d52db
AdamRaichu <91494019+AdamRaichu@users.noreply.github.com>
2022-06-10 14:51:56 -0400

    edited map1.text

--- maze/maps.js
+++ maze/maps.js
@@ -1,6 +1,6 @@
 var LEVEL_MAPS = {
   map1: {
-    text: "Welcome to my maze game!\nUse this map to get used to the controls. You can use either the arrow keys or WASD. When you're ready to move on, move to the 🏆 sign to complete the level.",
+    text: "Welcome to my maze game!\nUse this map to get used to the controls. You can use the arrow keys, the WASD keys, or the direction buttons above to control your character. When you're ready to move on, move to the 🏆 sign to complete the level.",
     matrix: [
       [1, 1, 1, 1, 1, 1, 1, 1],
       [1, 0, 0, 0, 0, 0, 0, 1],
