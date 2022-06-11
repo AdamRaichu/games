a0a10b3209937a042fd4a4f91f6a5e3d3387fae9
AdamRaichu <91494019+AdamRaichu@users.noreply.github.com>
2022-06-11 11:03:41 -0400

    continued work on portals

--- maze/constructor.js
+++ maze/constructor.js
@@ -27,7 +27,7 @@ MAP.prototype.generate = function () {
       } else if (e.innerText === "k") {
         e.innerText = "🗝️";
       } else if (e.innerText === "p") {
-        e.innerText = "🌀"
+        e.innerText = "🌀";
       }
     }
   }
@@ -89,7 +89,8 @@ PLAYER.prototype.move = function (dir) {
   if (this.map.get(this.x, this.y) === "p") {
     for (var c3 = 0; c3 < this.map.portals.length; c3++) {
       if (this.map.portals[c3].from === [this.x, this.y]) {
-
+        this.x = this.map.portals[c3].to[0];
+        this.y = this.map.portals[c3].to[1];
       }
     }
   }
@@ -100,6 +101,7 @@ PLAYER.prototype.move = function (dir) {
     LEVEL_MAPS = JSON.parse(_LEVEL_MAPS);
     document.getElementById("main").style.display = "none";
     document.getElementById("victory").style.display = "block";
+    document.getElementById("keys").innerHTML = "";
     return;
   }
 
