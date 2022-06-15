d8d042b14650a10e1446878027f0f9ee4229610e
AdamRaichu <91494019+AdamRaichu@users.noreply.github.com>
2022-06-15 19:44:53 -0400

    add check of equal row lengths

--- validation.js
+++ validation.js
@@ -12,12 +12,20 @@ module.exports = function () {
     eval(content);
 
     var keys = Object.keys(LEVEL_MAPS);
-    for (c = 0; c < keys.length; c++) {
+    for (var c = 0; c < keys.length; c++) {
       var o = LEVEL_MAPS[keys[c]];
       if (o.mapName !== keys[c]) {
-        console.error(`LEVEL_MAPS["${keys[c]}"].mapName does not equal "${keys[c]}"`);
+        console.error(`LEVEL_MAPS["${keys[c]}"].mapName DOES NOT equal "${keys[c]}"`);
       } else {
         console.debug(`LEVEL_MAPS["${keys[c]}"].mapName does equal "${keys[c]}"`)
+      }
+
+      for (var c2 = 1; c2 < o.matrix.length; c2++) {
+        if (o.matrix[c2].length !== o.matrix[c2 - 1].length) {
+          console.error(`LEVEL_MAPS["${keys[c]}"].matrix[${c2}].length DOES NOT equal LEVEL_MAPS["${keys[c]}"].matrix[${c2}].length`)
+        } else {
+          console.debug(`LEVEL_MAPS["${keys[c]}"].matrix[${c2}].length does equal LEVEL_MAPS["${keys[c]}"].matrix[${c2 - 1}].length`)
+        }
       }
     }
   })
