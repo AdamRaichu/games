1c311dc6d0d6fba92f0f2552e6cb58e3baf55e21
AdamRaichu <91494019+AdamRaichu@users.noreply.github.com>
2022-06-15 19:16:55 -0400

    add actual check statement

--- validation.js
+++ validation.js
@@ -9,6 +9,16 @@ module.exports = function () {
     }
 
     var content = buff.toString();
-    console.log(content);
+    eval(content);
+
+    var keys = Object.keys(LEVEL_MAPS);
+    for (c = 0; c < keys.length; c++) {
+      var o = LEVEL_MAPS[keys[c]];
+      if (o.mapName !== keys[c]) {
+        core.error(`LEVEL_MAPS["${keys[c]}"].mapName does not equal "${keys[c]}"`);
+      } else {
+        core.debug(`LEVEL_MAPS["${keys[c]}"].mapName does equal "${keys[c]}"`)
+      }
+    }
   })
 }
