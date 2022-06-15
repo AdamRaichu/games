f7f2af12811f9ac5f9162c9dc5baf636c7574362
AdamRaichu <91494019+AdamRaichu@users.noreply.github.com>
2022-06-15 18:49:58 -0400

    add skeleton validaton workflow

---
+++ validation.js
@@ -0,0 +1,14 @@
+module.exports = function () {
+  const fs = require("fs");
+
+  fs.readFile("/maze/maps.js", (err, buff) => {
+    if (err) {
+      console.error("[!!! ----- validation.js failed ----- !!!]");
+      console.error(err);
+      return;
+    }
+
+    var content = buff.toString();
+    console.log(content);
+  })
+}
