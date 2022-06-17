d36fa978976b28b60ba2cf613c20c52b6adee054
AdamRaichu <91494019+AdamRaichu@users.noreply.github.com>
2022-06-17 12:14:46 -0400

    add index.html validation

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 [![Validate maps.js](https://github.com/AdamRaichu/games/actions/workflows/validate.yml/badge.svg?event=push)](https://github.com/AdamRaichu/games/actions/workflows/validate.yml)
 
-This badge displays the status of the validation checks of [maze/maps.js](/maze/maps.js) as run by [validation.js](validation.js).
+This badge displays the status of the validation checks of [maze/maps.js](/maze/maps.js) and [maze/index.html](/maze/index.html) as run by [validation.js](validation.js).
--- validation.js
+++ validation.js
@@ -1,17 +1,23 @@
 module.exports = function () {
   const fs = require("fs");
 
+
+  // maps.js validation
   try {
-    var content = fs.readFileSync("maze/maps.js", "utf8")
+    var MAPS_JS = fs.readFileSync("maze/maps.js", "utf8")
   } catch (err) {
     if (err) {
       console.error("[!!! ----- validation.js failed ----- !!!]");
       console.error(err);
-      return false;
+      return {
+        isValid: false,
+        file: "validation.js",
+        reason: "validation.js encountered an unexpected error when parsing maps.js"
+      };
     }
   }
 
-  eval(content);
+  eval(MAPS_JS);
 
   var keys = Object.keys(LEVEL_MAPS);
   for (var c = 0; c < keys.length; c++) {
@@ -20,6 +26,7 @@ module.exports = function () {
       console.error(`LEVEL_MAPS["${keys[c]}"].mapName DOES NOT equal "${keys[c]}"`);
       return {
         isValid: false,
+        file: "maps.js",
         reason: `LEVEL_MAPS['${keys[c]}'].mapName DOES NOT equal '${keys[c]}'. In other words, I forgot to change an important value while copy-pasting.`
       };
     } else {
@@ -31,6 +38,7 @@ module.exports = function () {
         console.error(`LEVEL_MAPS["${keys[c]}"].matrix[${c2}].length DOES NOT equal LEVEL_MAPS["${keys[c]}"].matrix[${c2 - 1}].length`)
         return {
           isValid: false,
+          file: "maps.js",
           reason: `LEVEL_MAPS['${keys[c]}'].matrix[${c2}].length DOES NOT equal LEVEL_MAPS['${keys[c]}'].matrix[${c2 - 1}].length. In other words, one of the maps has rows of inequal length.`
         };
       } else {
@@ -39,8 +47,37 @@ module.exports = function () {
     }
   }
 
+
+  // index.html validation
+  try {
+    var INDEX = fs.readFileSync("maze/index.html", "utf8")
+  } catch (err) {
+    if (err) {
+      console.error("[!!! ----- validation.js failed ----- !!!]");
+      console.error(err);
+      return {
+        isValid: false,
+        file: "validation.js",
+        reason: "validation.js encountered an unexpected error when parsing index.html"
+      };
+    }
+  }
+
+  if (INDEX.match(/<\/option>/g).length === Object.keys(LEVEL_MAPS).length) {
+    console.debug("There are as many <option> elements as there are keys under LEVEL_MAPS.");
+  } else {
+    console.error("The number of <option> elements DOES NOT equal the number of keys under LEVEL_MAPS.")
+    return {
+      isValid: false,
+      file: "index.html",
+      reason: "The number of <option> elements does not match the number of keys under LEVEL_MAPS."
+    }
+  }
+
+
   return {
     isValid: true,
+    file: "validation.js",
     reason: "All checks passed."
   };
 }
