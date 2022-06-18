95e896a047914c2b67dee9f2450f68dc40d3ea25
AdamRaichu <91494019+AdamRaichu@users.noreply.github.com>
2022-06-18 17:32:11 -0400

    converted footer links to popups

--- maze/footer.js
+++ maze/footer.js
@@ -2,7 +2,7 @@
   var b = document.querySelectorAll("#footer button");
   for (var c4 = 0; c4 < b.length; c4++) {
     b[c4].addEventListener("click", function () {
-      window.open(this.dataset.link);
-    })
+      createPopup(this.dataset.html);
+    });
   }
 }
