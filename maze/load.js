d3b0246b0924307b9b0d08cea2843f5b76215d39
AdamRaichu <91494019+AdamRaichu@users.noreply.github.com>
2022-06-21 21:12:30 -0400

    fixed styling

--- maze/load.js
+++ maze/load.js
@@ -78,7 +78,7 @@ $.getJSON("isValid.json", function (data) {
     var h = `
       <h1>Warning!</h1>
       <p>According to automated checks, there is a problem with the code behind this game. Here is the information passed from the validation software:</p>
-      <pre>
+      <pre class="hljs">
         <code>${hljs.highlight(JSON.stringify(data, null, 2), {
       language: "json"
     }).value}</code>
