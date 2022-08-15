b730bb5cb0ead736f88d53dd8f1d37accde1d92f
AdamRaichu <91494019+AdamRaichu@users.noreply.github.com>
2022-08-15 13:34:24 -0400

    switch to root level popup files
    see AdamRaichu/adamraichu.github.io

--- maze/load.js
+++ maze/load.js
@@ -31,48 +31,6 @@ if (defaultMap === undefined) {
 }
 loadMap(defaultMap);
 
-function createPopup(HTML, options) {
-  if (typeof options !== "object") {
-    options = {};
-  }
-
-  var dim = document.createElement("div");
-  dim.classList.add("popup-dimmer");
-  if (typeof options.dim === "object") {
-    if (typeof options.dim.color === "string") {
-      dim.style.backgroundColor = options.dim.color;
-    }
-  }
-  document.body.appendChild(dim);
-
-  var wrapper = document.createElement("div");
-  wrapper.classList.add("popup-wrapper");
-
-  var popup = document.createElement("div");
-  popup.classList.add("popup");
-  popup.innerHTML = HTML;
-  if (typeof options.popup === "object") {
-    if (typeof options.popup.color === "string") {
-      popup.style.backgroundColor = options.popup.color
-    }
-  }
-  wrapper.appendChild(popup);
-
-  var dismiss = document.createElement("div");
-  dismiss.classList.add("popup-dismiss");
-  var dismissButton = document.createElement("button");
-  dismissButton.innerText = "x";
-  dismiss.appendChild(dismissButton);
-  wrapper.appendChild(dismiss);
-
-  document.body.appendChild(wrapper);
-
-  dismissButton.addEventListener("click", function () {
-    dim.remove();
-    wrapper.remove();
-  })
-}
-
 $.getJSON("isValid.json", function (data) {
   console.log("LEVEL_MAPS validation status:\n" + JSON.stringify(data, null, 2));
   if (!data.isValid) {
